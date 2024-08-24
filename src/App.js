860200f5b4c25bc11984d15d3cabe0e7e0c5195f
OwlDeev <alfonso.sandoval.wp@gmail.com>
2024-08-25 01:11:11 +0200

    create at-image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import AtImage from './components/atoms/at-image/at-image.tsx';
+const imageUrl = '/assets/imgs/hero-test.jpg';
 
 function App() {
+
+  const imgProps = {
+    urlAction: "",
+    title: "",
+    media: imageUrl,
+    altText: ""
+  }
+
+  console.log(imgProps)
   return (
     <div className="">
-      Chirimoya Crochet
+      <AtImage media={imgProps.media}>
+      </AtImage>
     </div>
   );
 }

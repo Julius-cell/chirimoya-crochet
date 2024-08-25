0d29609edc0fe9a0ebc23373c542ef9b32012522
juliocid <julio.cid@femsasalud.com>
2024-08-25 03:04:32 +0200

    feat: add button styles

--- src/components/atoms/at-button/at-button.tsx
+++ src/components/atoms/at-button/at-button.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
 
 interface AtButtonProps {
   text: string;
   urlAction: number;
-  color: string;
+  primaryColor: string;
+  secondaryColor: string;
 }
 
-const AtButton: React.FC<AtButtonProps> = ({ text, color, urlAction }) => {
+const AtButton: React.FC<AtButtonProps> = ({
+  text = "Descubre la colección",
+  primaryColor = "#027385",
+  secondaryColor = "white",
+  urlAction,
+}) => {
   return (
-    <button>{text}</button>
+    <button className="text-white border py-2 border-white rounded-md w-full bg-[#027385] hover:bg-white hover:text-[#027385] hover:border-[#027385] hover:scale-105 transition-all">
+      {text}
+    </button>
   );
 };
 
